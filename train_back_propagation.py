600eaa653b10b109fbcd5fd889e4bdd9f4de8b36
KAI_desktop <ms0346648@gmail.com>
2019-03-26 03:33:44 +0800

    - start traing well

--- train_back_propagation.py
+++ train_back_propagation.py
@@ -1,9 +1,13 @@
 import sys
+import os
 import json
+from datetime import datetime
 
 import numpy as np
 
-CHECK_POINT = 5000
+CHECK_POINT = 50000
+LOGGER = False
+DEBUG = False
 
 def sigmoid(x):
     return 1.0 / (1.0 + np.exp(-x))
@@ -12,12 +16,18 @@ def derivative_sigmoid(x):
     return np.multiply(x, 1.0 - x)
 
 def loss_func(outputs, ground_truth):
-    mse = np.square(ground_truth - outputs)
+    mse = np.square(ground_truth - outputs) / 2
     return mse
 
 def derivative_loss_func(outputs, ground_truth):
-    derivative_mse = 2 * (outputs - ground_truth)
+    if len(outputs) != len(ground_truth):
+        raise Exception('ground_truth shape different from outputs')
+    derivative_mse = (outputs - ground_truth)
     return derivative_mse
+
+def debug_log(*args):
+    if LOGGER:
+        print(*args)
 
 class NeuralNet():
     def __init__(self, l_rate=0.5):
@@ -39,7 +49,7 @@ class NeuralNet():
         for idx, layer in enumerate(self.layers):
             if idx == len(self.layers)-1:
                 layer_input = layer.cal_outputs(layer_input)
-                # print(layer_input, gt)
+                debug_log(layer_input, gt)
                 return loss_func(layer_input, gt)
             layer_input = layer.cal_outputs(layer_input)
         # return layer_input
@@ -50,7 +60,7 @@ class NeuralNet():
         '''
         pre_delta = np.array([])
         for layer_idx in range(len(self.layers)-1, -1, -1):
-            # print('Layer ', layer_idx)
+            debug_log('Layer ', layer_idx)
             if len(pre_delta) == 0:
                 error = derivative_loss_func(self.layers[layer_idx].outputs, ground_truths)
                 delta = self.layers[layer_idx].cal_delta(error)
@@ -60,7 +70,7 @@ class NeuralNet():
             pre_delta = delta
         for idx, l in enumerate(self.layers):
             l.fix_weights(self.learning_rate)
-            # print("layer", idx, 'new_weights:', l.weights)
+            debug_log("layer", idx, 'new_weights:', l.weights)
 
     def insert_weight(self, weight_dict):
         for val in weight_dict.values():
@@ -81,13 +91,14 @@ class Neurons():
     '''
         one layer include weight and outputs(delta, z)
     '''
-    def __init__(self, w_shape=None):
+    def __init__(self, w_shape=None, bias=None):
         self.weights = np.array([])
         if w_shape is not None:
             self.weights = self.init_weights(w_shape)
         self.delta = np.array([])
         self.outputs = []
         self.inputs = []
+        self.bias = 0 if bias is None else bias
 
     def get_weight(self):
         return self.weights
@@ -106,33 +117,40 @@ class Neurons():
         for i in range(count):
             res.append(i)
         if count == 2:
-            res = [0.5, 0.6]
+            res = [0.4, 0.5, 0.45, 0.55]
         elif count == 4:
-            res = [0.5, 0.1, 0.2, 0.5]
-        return np.reshape(res, weights_shape)
+            res = [0.15, 0.25, 0.2, 0.3]
+        if DEBUG:
+            return np.reshape(res, (2,2))
         return np.random.uniform(0.001, 1, weights_shape)
 
     def cal_outputs(self, inputs):
         if len(inputs) != len(self.weights):
             raise Exception('inputs error')
         self.inputs = np.reshape(inputs, (len(inputs),1))
-        self.outputs = sigmoid(np.matmul(self.weights.T, inputs))
-        # print(self.weights.T, '*', self.inputs, '=', self.outputs)
-        self.outputs = self.outputs.reshape((len(self.outputs),1))
+        self.outputs = sigmoid(np.matmul(self.weights.T, inputs) + self.bias)
+        debug_log(self.weights.T, '*', self.inputs, '+', self.bias, '=', self.outputs)
+        # self.outputs = self.outputs.reshape((len(self.outputs),1))
         return self.outputs
 
     def cal_delta(self, error):
-        # print('delta: ', error, '*', derivative_sigmoid(self.outputs))
-        # print('self.outputs: ', self.outputs)
+        debug_log('delta: ', error, '*', derivative_sigmoid(self.outputs))
+        debug_log('self.outputs: ', self.outputs)
         self.delta = np.multiply(error, derivative_sigmoid(self.outputs))
         if not isinstance(self.delta, type(np.array([]))):
             self.delta = np.array([self.delta])
-        # print( '=', self.delta)
+        debug_log( '=', self.delta)
         return self.delta
     
     def fix_weights(self, learning_rate):
-        # print('fix_weights', self.weights, learning_rate, '*', self.delta, 'X', self.inputs)
-        self.weights = self.weights - learning_rate * np.multiply(self.delta, self.inputs)
+        if self.weights.size > self.delta.size:
+            length = len(self.delta)
+            delta = np.reshape(self.delta, (1, length))
+            delta = np.repeat(delta, length, axis=0)
+        else:
+            delta = self.delta
+        debug_log('fix_weights', self.weights, learning_rate, '*', delta, 'X', self.inputs)
+        self.weights = self.weights - learning_rate * np.multiply(delta, self.inputs)
 
 def generate_linear(n=100):
     pts = np.random.uniform(0, 1, (n, 2))
@@ -160,7 +178,7 @@ def generate_XOR_easy():
     return np.array(inputs), np.array(labels).reshape(21, 1)
 
 def gen_input():
-    return [[.1, .1], [.5, .5]], np.array([1, 0])
+    return [[.1, .1], [.1, .1], [.5, .5]], np.array([1, 1, 0])
 
 class NumpyEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -168,16 +186,21 @@ class NumpyEncoder(json.JSONEncoder):
             return obj.tolist()
         return json.JSONEncoder.default(self, obj)
         
-def train(file_name):
+def train(file_name, learning_rate):
     # x, y = generate_linear(100)
-    # x, y = generate_XOR_easy()
-    x, y = gen_input()
+    x, y = generate_XOR_easy()
+    if DEBUG:
+        x, y = gen_input()
     w_shapes = [[2,2], [2,2], [2,1]]
-    result_NN = NeuralNet(l_rate=0.8)
+    result_NN = NeuralNet(l_rate=learning_rate)
     # init weights
-    for w in w_shapes:
+    for idx,w in enumerate(w_shapes):
         layer = Neurons(w)
         result_NN.add_layer(layer)
+    
+    folder_name = datetime.now().strftime('%m_%d_%H-%M')
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
     count = 1
     while count:
         loss_list = []
@@ -189,7 +212,9 @@ def train(file_name):
         if count % CHECK_POINT == 0:
             print('epoch', count, 'Loss with MSE', np.max(loss_list))
             print('Weights:', result_NN.get_w())
-            with open(file_name + '.json', 'w') as f:
+            with open(os.path.join(folder_name, file_name + CHECK_POINT + '.json'), 'w') as f:
+                json.dump({'weight': result_NN.get_w()}, f, cls=NumpyEncoder)
+            with open(os.path.join(folder_name, file_name + '.json'), 'w') as f:
                 json.dump({'weight': result_NN.get_w()}, f, cls=NumpyEncoder)
         count += 1
 
@@ -205,8 +230,8 @@ def test(file_name):
     print(list(zip(res, y)))
 
 if __name__ == "__main__":
-    file_name = 'model'
-    train(file_name)
+    file_name = 'XOR08'
+    train(file_name, 0.8)
     # test(file_name)
     sys.stdout.flush()
     sys.exit()
